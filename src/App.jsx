80df452e2622d4508c28a5d5a6f1fb7a7c16b229
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2025-01-04 20:21:07 +0100

    add ButtonExamples component and integrate it into App

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import ButtonExamples from './components/button-examples/ButtonExamples'
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+        <ButtonExamples />
+
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
---
+++ src/components/button-examples/ButtonExamples.jsx
@@ -0,0 +1,14 @@
+// ButtonExamples.jsx
+
+import React from 'react';
+
+const ButtonExamples = () => {
+    return (
+        <div>
+            <h1>Button Examples</h1>
+            <sub>These are some examples of buttons</sub>
+        </div>
+    );
+};
+
+export default ButtonExamples;
